4adb3678253349ce78c118a6531ca06afab9b708
Micah Young <micah@young.io>
2013-03-13 23:33:40 -0400

    Add select2 for selects

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Workinit
 A few hacks for Work.com
 
+* select tags with many options become [select2](http://ivaynberg.github.com/select2/).
+
 ## License
 MIT
--- workinit.js
+++ workinit.js
@@ -1,23 +1,37 @@
 (function($) {
-  var minOptionCount = 10;
-
   var compareOptions = function(a,b) {
     var result = a.value.toLowerCase() > b.value.toLowerCase();
     return (result) ? 1 : -1;
   };
 
-  var sortSelectOptions = function() {
+  var sortSelectOptions = function($select) {
+    var $sorted_options = $select.children("option").sort(compareOptions);
+    $select.append($sorted_options);
+  };
+
+  var initSelect2 = function($select) {
+    var selectWidth = 400;
+    var extId = chrome.i18n.getMessage("@@extension_id");
+    var select2CssUrl = "chrome-extension://" + extId + "/lib/select2-3.3.1/select2.css";
+    $("<link rel='stylesheet' type='text/css' href='" + select2CssUrl + "'>").appendTo("head");
+
+    $select.css({width: selectWidth});
+    $select.select2();
+  };
+
+  $(document).on("mouseover", "select", function(){
+    var minOptionCount = 10;
     $select = $(this);
 
-    if ($select.data("workinit.sorted") || $select.children("option").size() < minOptionCount) {
+    if ($select.data("workinit.sorted") === true ||
+          $select.children("option").size() < minOptionCount) {
       return;
     }
 
     $select.data("workinit.sorted", true);
 
-    var $sorted_options = $select.children("option").sort(compareOptions);
-    $select.append($sorted_options);
-  };
+    sortSelectOptions($select);
+    initSelect2($select);
+  });
 
-  $(document).on("mousedown", "select", sortSelectOptions);
 })(jQuery);
